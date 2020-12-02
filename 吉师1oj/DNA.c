cc31daaddf27318ba3be700ac7e98c4ddb01265f
1908030108 <73279673+1908030108@users.noreply.github.com>
2020-12-02 14:47:12 +0800

    Update DNA.c

--- 吉师1oj/DNA.c
+++ 吉师1oj/DNA.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 int main(void){
-    int n, a, b;
+    int n,a,b;
     scanf("%d",&n);
     while(n--){
         scanf("%d%d",&a,&b);
-
+        int flag = 1;
+        while(b--){
+            for(int i=0; i<a; i++)
+               if(i!=0 || flag)
+            {
+            for(int j=0; j<a; j++)
+               if(j==i || j==a-i-1)
+                    printf("X");
+                else
+                printf(" ");
+            printf("\n");
+            flag = 0;
+            }
+        }
+        if(n!=0)
+                printf("\n");
     }
+
 return 0;
 }
