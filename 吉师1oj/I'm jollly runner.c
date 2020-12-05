dfda837ea3fd3697825785570f8a3dcec734fb37
1908030108 <73279673+1908030108@users.noreply.github.com>
2020-12-05 16:46:51 +0800

    Update I'm jollly runner.c

--- 吉师1oj/I'm jollly runner.c
+++ 吉师1oj/I'm jollly runner.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
 #include <math.h>
-#define M 10000
+#define M 100000
 
-int abs(int n){
+int abs(int n)
+{
     if(n < 0) return -n;
     else return n;
 }
-int a[M];
+
 int main(void)
 {
-    int n, i;
-    while(~scanf("%d",&n)){
-        int flag = 0;
-       for(i = 0; i < M; i++)
-          a[i] = 0;
-       for(i = 0; i < n; i++)
+    int n, i, j, flag;
+     while(scanf("%d",&n)!=EOF)
+     {
+         flag = 0;
+        int a[M], b[M];
+        for(i = 0; i < n; i++)
             scanf("%d",&a[i]);
-       for(i = 1; i < n; i++){
-        if((abs(a[i])-abs(a[i-1])) <= 0 || (abs(a[i])-abs(a[i-1])) >= n){
-              flag = 1;
-              break;
+        for(i = 1; i < n; i++)
+        {
+            b[i] = abs(a[i]-a[i-1]);
         }
-       }
-       if(flag == 0) printf("Jolly\n");
-       else printf("Not Jolly\n");
-    }
-return 0;
+        for(i = 1; i <= n; i++)
+        {
+            for(j = 1; j <= n - i - 1; j++)
+            {
+                if(b[j] > b[j+1])
+                {
+                int temp = b[j];
+                    b[j] = b[j+1];
+                    b[j+1] = temp;
+                }
+            }
+        }
+//        for(i = 1; i < n; i++)
+//            printf("%d ",b[i]);
+        for(i = 1; i < n; i++)
+        {
+            if(b[i] != i)
+            {
+             flag = 1;
+             printf("Not jolly\n");
+             break;
+            }
+        }
+        if(flag == 0)
+        printf("Jolly\n");
+     }
+    return 0;
 }
+
